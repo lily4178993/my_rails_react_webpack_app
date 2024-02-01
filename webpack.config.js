321822769da8e0549b7226fb3b173d8db63f8469
lily4178993 <nellytelli@gmail.com>
2024-02-01 17:21:09 +0000

    Add Message model and update webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,37 @@ const path    = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-  mode: "production",
-  devtool: "source-map",
+  mode: 'production',
+  devtool: 'source-map',
   entry: {
-    application: "./app/javascript/application.js"
+    application: './app/javascript/application.js',
   },
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader'],
-      }
-    ]
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
   },
   output: {
-    filename: "[name].js",
-    sourceMapFilename: "[file].map",
-    chunkFormat: "module",
-    path: path.resolve(__dirname, "app/assets/builds"),
+    filename: '[name].js',
+    sourceMapFilename: '[file].map',
+    chunkFormat: 'module',
+    path: path.resolve(__dirname, 'app/assets/builds'),
   },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1
-    })
-  ]
-}
+      maxChunks: 1,
+    }),
+  ],
+};
