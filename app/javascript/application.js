c5faae0d7f872672d1d0ae17297a428d4a22f1c1
lily4178993 <nellytelli@gmail.com>
2024-02-01 17:20:05 +0000

    Refactor application.js to use React and Redux

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,20 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+/* import "@hotwired/turbo-rails"
+import "./controllers" */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store/store';
+import App from './components/App';
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>,
+    document.getElementById('root')
+  );
+});
