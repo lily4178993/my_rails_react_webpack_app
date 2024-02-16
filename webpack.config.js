82e89d3d2d56c8ea00e28be6c44943b5514afb6e
lily4178993 <nellytelli@gmail.com>
2024-02-16 18:12:33 +0000

    Refactor webpack.config.js for production and development environments

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,35 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require('path');
+const webpack = require('webpack');
 
-module.exports = {
-  mode: 'production',
-  devtool: 'source-map',
-  entry: {
-    application: './app/javascript/application.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-          },
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+    entry: {
+      application: './app/javascript/application.js',
+      vendor: ['react', 'react-dom', 'redux', 'react-redux'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
         },
-      },
+      ],
+    },
+    output: {
+      filename: '[name].js',
+      sourceMapFilename: '[file].map',
+      chunkFormat: 'module',
+      path: path.resolve(__dirname, 'app/assets/builds'),
+    },
+    plugins: [
+      new webpack.optimize.LimitChunkCountPlugin({
+        maxChunks: 1,
+      }),
     ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  output: {
-    filename: '[name].js',
-    sourceMapFilename: '[file].map',
-    chunkFormat: 'module',
-    path: path.resolve(__dirname, 'app/assets/builds'),
-  },
-  plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    }),
-  ],
+  };
 };
